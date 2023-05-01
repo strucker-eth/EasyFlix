ed21d5575444370d1504cfe400be705a12b38d3c
Strucker.eth <aberamacarleton@gmail.com>
2023-05-02 01:39:41 +0300

    Linking to movie detail age

--- src/components/Peek.jsx
+++ src/components/Peek.jsx
@@ -3,16 +3,16 @@ import Image from "next/image";
 
 export const Peek = () => {
     return (
-      <div className="px-3 py-5 sm:max-w-xl gap-1 md:max-w-full lg:max-w-screen-xl">
+      <div className="px-3 py-5 sm:max-w-xl w-10/12 gap-1 ">
 
         <div class="relative m-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
-  <Link class="relative  flex h-30 overflow-hidden rounded-xl" href="#">
+  <Link class="relative  flex h-30 overflow-hidden rounded-xl" href="/Movies">
     <Image class="object-cover -40 w-full" src="/img/chariot.jpg" width={300}
               height={300} alt="product image" />
     <span class="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white">39% OFF</span>
   </Link>
   <div class="mt-4 px-2 pb-5">
-    <Link href="#">
+    <Link href="/Movies">
       <h5 class="text-xl tracking-tight text-slate-900">Nike Air MX Super 2500 - Red</h5>
     </Link>
     <div class="mt-2 mb-5 flex items-center justify-between">
