d31c64b0d0cec3046622689a463ecc6d5dda1936
Strucker.eth <aberamacarleton@gmail.com>
2023-05-01 23:39:42 +0300

    Hero Images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -82,25 +82,37 @@ export const Landing = () => {
           </div>
         </div>
         <div className="flex items-center justify-center -mx-4 lg:pl-8">
+        <div className="px-3">
+            <Image
+              className="object-cover w-40 h-40 rounded shadow-lg sm:h-64 xl:h-80 sm:w-64 xl:w-80"
+              src="/img/HQ.jpg"
+              alt="" width={300}
+              height={300}
+            ></Image>
+          </div>
           <div className="flex flex-col items-end px-3">
-            <img
+            <Image
               className="object-cover mb-6 rounded shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56"
-              src="https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-              alt=""
-            />
-            <img
+              src="/img/WestWorld.jpg"
+              alt="" width={300}
+              height={300}
+            ></Image>
+            <Image
               className="object-cover w-20 h-20 rounded shadow-lg sm:h-32 xl:h-40 sm:w-32 xl:w-40"
-              src="https://images.pexels.com/photos/3182812/pexels-photo-3182812.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-              alt=""
-            />
+              src="/img/Strange.jpg"
+              alt=""  width={300}
+              height={300}
+            ></Image>
           </div>
           <div className="px-3">
-            <img
+            <Image
               className="object-cover w-40 h-40 rounded shadow-lg sm:h-64 xl:h-80 sm:w-64 xl:w-80"
-              src="https://images.pexels.com/photos/3182739/pexels-photo-3182739.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
-              alt=""
-            />
+              src="/img/Wakanda.jpg"
+              alt="" width={300}
+              height={300}
+            ></Image>
           </div>
+
         </div>
       </div>
     </div>
