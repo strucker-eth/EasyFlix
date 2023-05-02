36e5b5fb4959b3423f8ee0fdf0ef678293bc1244
Strucker.eth <aberamacarleton@gmail.com>
2023-05-02 04:34:33 +0300

    Hero Icon resizing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,8 +6,8 @@ export const Landing = () => {
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="grid gap-10 lg:grid-cols-2">
         <div className="flex flex-col justify-center md:pr-8 xl:pr-0 lg:max-w-lg">
-          <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-blue-accent-400">
-          <Image src="/img/logo.png" width={50}
+          <div className="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-blue-accent-400">
+          <Image src="/img/logo.png" width={30}
               height={50} alt=""></Image>
           </div>
           <div className="max-w-xl mb-6">
