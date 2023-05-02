98c664bed6cb065df7d593ce57c3df1a38a4d5c3
Strucker.eth <aberamacarleton@gmail.com>
2023-05-02 20:39:26 +0300

    amazon icon fix

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -88,7 +88,7 @@ export const Checkout = () => {
         <input className="peer hidden" id="radio_2" type="radio" name="radio" checked />
         <span className="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
         <label className="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4" for="radio_2">
-          <Image className="w-14 object-contain" src="/img/Amazon.png" alt="" width={300}
+          <Image className="w-14 object-contain" src="/img/amazon.png" alt="" width={300}
               height={300}></Image>
           <div className="ml-5">
             <span className="mt-2 font-semibold">Amazon</span>
